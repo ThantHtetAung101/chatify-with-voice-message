fcd177b463bc588341145411bede2163786d0abf
Thant Htet Aung <114596700+ThantHtetAung101@users.noreply.github.com>
2024-09-04 13:49:48 +0630

    config value extract wrong fixed

--- src/Http/Controllers/MessagesController.php
+++ src/Http/Controllers/MessagesController.php
@@ -146,7 +146,7 @@ class MessagesController extends Controller
             ]);
             $messageData = Chatify::parseMessage($message);
             if (Auth::user()->id != $request['id']) {
-                $channel_name = config('chatify.pusher.channel_name');
+                $channel_name = config('chatify.channel_name');
                 Chatify::push($channel_name, 'messaging', [
                     'from_id' => Auth::user()->id,
                     'to_id' => $request['id'],
