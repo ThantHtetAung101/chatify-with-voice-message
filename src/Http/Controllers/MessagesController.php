6ae992bc005c711a9a26b6177181c93d0680c317
Thant Htet Aung <114596700+ThantHtetAung101@users.noreply.github.com>
2024-09-09 12:07:36 +0630

    bug fix

--- src/Http/Controllers/MessagesController.php
+++ src/Http/Controllers/MessagesController.php
@@ -146,7 +146,7 @@ class MessagesController extends Controller
             ]);
             $messageData = Chatify::parseMessage($message);
             if (Auth::user()->id != $request['id']) {
-                $channel_name = config('chatify.channel_name');
+                $channel_name = "private-chatify." . $request['id'];
                 Chatify::push($channel_name, 'messaging', [
                     'from_id' => Auth::user()->id,
                     'to_id' => $request['id'],
