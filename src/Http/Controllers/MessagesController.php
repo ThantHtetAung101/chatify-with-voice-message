a16293d927614840a7bc1251abb02b9108aa2a4c
Thant Htet Aung <114596700+ThantHtetAung101@users.noreply.github.com>
2025-01-08 16:15:24 +0630

    message controller for dashboard is updated to set admin 1

--- src/Http/Controllers/MessagesController.php
+++ src/Http/Controllers/MessagesController.php
@@ -237,10 +237,10 @@ class MessagesController extends Controller
                 ->orOn('ch_messages.to_id', '=', 'users.id');
         })
             ->where(function ($q) {
-                $q->where('ch_messages.from_id', Auth::user()->id)
-                    ->orWhere('ch_messages.to_id', Auth::user()->id);
+                $q->where('ch_messages.from_id', 1)
+                    ->orWhere('ch_messages.to_id', 1);
             })
-            ->where('users.id', '!=', Auth::user()->id)
+            ->where('users.id', '!=', 1)
             ->select('users.*', DB::raw('MAX(ch_messages.created_at) max_created_at'))
             ->orderBy('max_created_at', 'desc')
             ->groupBy('users.id')
