f6b95b87a8d7cb8404e956ea4e438b2895b92727
Thant Htet Aung <114596700+ThantHtetAung101@users.noreply.github.com>
2025-03-22 11:43:18 +0630

    adding debugging dd to noti send

--- src/Http/Controllers/MessagesController.php
+++ src/Http/Controllers/MessagesController.php
@@ -543,6 +543,7 @@ class MessagesController extends Controller
                 ])->post($url, [
                     'message' => $data
                 ]);
+                dd($response->json());
 
                 return true;
             }
